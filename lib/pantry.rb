c477fb3b5f4fd66fb585152201e75043d2f8a6aa
alex549us3 <alex549us3@gmail.com>
2017-04-18 09:41:31 -0600

    iteration one done

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -1,3 +1,26 @@
 class Pantry
+  attr_accessor :stock
+
+  def initialize
+    @stock = {}
+  end
+
+  def stock_check(item)
+    if !stock[item].nil?
+      stock[item]
+    else
+      0
+    end
+  end
+
+  def restock(item, number)
+    if !stock[item].nil?
+      stock[item] = (stock[item] + number)
+    else
+      stock[item] = number
+    end
+  end
+
+
 
 end
--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -4,4 +4,29 @@ require 'minitest/pride'
 
 class PantryTest < Minitest::Test
 
+  def setup
+    @pantry = Pantry.new
+  end
+
+  def test_it_initializes
+    assert_instance_of Pantry, @pantry
+  end
+  
+  def test_it_starts_with_no_stock
+    assert_instance_of Hash, @pantry.stock
+  end
+
+  def test_it_can_check_stock
+    assert_equal 0, @pantry.stock_check('Cheese')
+  end
+
+  def test_it_can_add_to_stock
+    assert_equal 0, @pantry.stock_check('Cheese')
+    @pantry.restock('Cheese', 10)
+    assert_equal 10, @pantry.stock_check('Cheese')
+    @pantry.restock('Cheese', 20)
+    assert_equal 30, @pantry.stock_check('Cheese')
+  end
+
+
 end
